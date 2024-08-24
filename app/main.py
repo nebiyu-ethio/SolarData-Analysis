d6a3a2bc97f2114a9643f0926d511dce43b444b1
nebiyu-ethio <nebiyuethio@gmail.com>
2024-08-24 21:43:45 +0300

    Update the main app

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ if uploaded_file is not None:
     # Load the data into a DataFrame
     try:
         # Read the CSV file
-        data = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file)
         
         # Display the dataframe
       
